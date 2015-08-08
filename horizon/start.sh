9ff8956421a2756c633f5c289c91c7d84b5182b1
Ian Unruh <ianunruh@gmail.com>
2015-08-07 17:15:21 -0700

    Improve Horizon logging

--- horizon/start.sh
+++ horizon/start.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-ln -s /etc/openstack-dashboard/local_settings.py /opt/horizon/openstack_dashboard/local/local_settings.py
+set -eux
+
+ln -sf /etc/openstack-dashboard/local_settings.py /opt/horizon/openstack_dashboard/local/local_settings.py
 
 /opt/horizon/manage.py compress
+
+# http://serverfault.com/questions/599103/make-a-docker-application-write-to-stdout
+ln -sf /dev/stdout /var/log/apache2/access.log
+ln -sf /dev/stderr /var/log/apache2/error.log
 
 apache2ctl -DFOREGROUND
